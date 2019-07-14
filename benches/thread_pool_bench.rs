23b76e360c04eeee413423835a415e4a4f4b7e40
Jacob Zuo <chopinsky@live.com>
2019-07-14 17:29:46 -0500

    Better perf measurement

--- benches/thread_pool_bench.rs
+++ benches/thread_pool_bench.rs
@@ -28,7 +28,7 @@ fn single_bench(c: &mut Criterion) {
     shared_mode::initialize(black_box(16));
     let bound = black_box(400);
 
-    c.bench_function("base single mode measurement", |b| {
+    c.bench_function("base single mode measurement", move |b| {
         b.iter(|| {
             for num in 1..bound {
                 shared_mode::run(move || for _ in 1..num%16 { atomic::spin_loop_hint(); });
